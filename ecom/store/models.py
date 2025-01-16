44a22a4f7223a5d121af26bc507b7c2f86ccbaeb
chris likibi <ngouloulikibichris@gmail.com>
2025-01-16 16:05:46 +0800

    'commit'

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -26,6 +26,11 @@ class Product(models.Model):
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
     
+    # Add sale stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    
+    
     def __str__(self):
         return self.name
     
@@ -38,6 +43,7 @@ class Order(models.Model):
     phone = models.CharField(max_length=20, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
     status = models.BooleanField(default=False)
+    
     
     def __str__(self):
         return self.product
