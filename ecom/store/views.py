96d2b5f9dbe91a9f6764bc564d507561443b5c9f
chris likibi <ngouloulikibichris@gmail.com>
2025-01-17 21:19:13 +0800

    'commit'

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     products = Product.objects.all()
@@ -13,3 +15,13 @@ def about(request):
     return render(request, 'about.html', {
         
     })
+    
+
+def login_user(request):
+    return render(request, 'login.html', {
+        
+    })
+
+
+def logout_user(request):
+    pass
