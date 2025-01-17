5dfce64f119f021cf2da8080ddb5986925e799ab
chris likibi <ngouloulikibichris@gmail.com>
2025-01-17 21:51:09 +0800

    'commit'

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -18,10 +18,25 @@ def about(request):
     
 
 def login_user(request):
-    return render(request, 'login.html', {
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There a mistake with your username or password"))
+            return redirect('login')
+    else:        
+        return render(request, 'login.html', {
         
     })
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, ("You have been logged out, thanks for stopping by ...."))
+    return redirect('home')
